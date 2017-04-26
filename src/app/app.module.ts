dfa53c8d5ca10b242bf11e9cde408eaa68aada2d
Michael Cebrian <michael.cebrian@gmail.com>
2017-04-26 17:23:32 -0400

    Add KendoUI Demo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
-import { AppRoutingModule } from './app-routing.module';
+import { LayoutModule } from '@progress/kendo-angular-layout';
+import { InputsModule } from '@progress/kendo-angular-inputs';
+import { DropDownsModule } from '@progress/kendo-angular-dropdowns';
+import { PopupModule } from '@progress/kendo-angular-popup';
+import { DialogModule } from '@progress/kendo-angular-dialog';
+
 import { AppComponent } from './app.component';
+import { DemoComponent } from './demo/demo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DemoComponent
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
+    DialogModule,
     FormsModule,
     HttpModule,
-    AppRoutingModule
+    InputsModule,
+    LayoutModule,
+    DropDownsModule,
+    PopupModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/demo/demo.component.ts
@@ -0,0 +1,28 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ng-ui-demo',
+  templateUrl: './demo.component.html',
+  styleUrls: ['./demo.component.scss']
+})
+export class DemoComponent implements OnInit {
+  @Input() deep: number;
+  public maskedValue: string;
+  public sliderValue: number = 5;
+  public numericValue: number = 5;
+  public switchValue: boolean = false;
+  public min: number = 0;
+  public max: number = 10;
+  public smallStep: number = 1;
+  public listItems: Array<string> = ['Option 1', 'Option 2', 'Option 3'];
+  public pop: boolean = false;
+  public tool: boolean = false;
+  public dialog: boolean = false;
+  public popAlign: {} = { vertical: 'top', horizontal: 'right' };
+  public anchorAlign: {} = { vertical: 'top', horizontal: 'right' };
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
