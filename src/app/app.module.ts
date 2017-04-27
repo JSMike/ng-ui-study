e57779152567242ebf08267e47dae59bfe4ea28a
Michael Cebrian <michael.cebrian@gmail.com>
2017-04-27 12:03:35 -0400

    Add Angular-MDL Demo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,25 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
-import { AppRoutingModule } from './app-routing.module';
+import { MdlModule } from '@angular-mdl/core';
+import { MdlSelectModule } from '@angular-mdl/select';
+import { MdlPopoverModule } from '@angular-mdl/popover';
+
 import { AppComponent } from './app.component';
+import { DemoComponent } from './demo/demo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DemoComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
-    AppRoutingModule
+    MdlModule,
+    MdlPopoverModule,
+    MdlSelectModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/demo/demo.component.ts
@@ -0,0 +1,26 @@
+import { Component, Input } from '@angular/core';
+import { MdlSnackbarService } from '@angular-mdl/core';
+
+@Component({
+  selector: 'ng-ui-demo',
+  templateUrl: './demo.component.html',
+  styleUrls: ['./demo.component.scss']
+})
+export class DemoComponent {
+  @Input() deep: number;
+
+  constructor(private mdlSnackbarService: MdlSnackbarService) {}
+
+  showSnackbar() {
+    this.mdlSnackbarService.showSnackbar({
+      message: 'The Message',
+      action: {
+        handler: () => {
+          this.mdlSnackbarService.showToast('You hit the ok Button');
+        },
+        text: 'OK'
+      }
+    });
+  }
+
+}
