43746438a8ffc8c4d0154cf4cd041b6bf506d925
Michael Cebrian <michael.cebrian@gmail.com>
2017-05-06 22:04:02 -0400

    Update pages to make sure everything's uniform

--- src/app/demo/demo.component.ts
+++ src/app/demo/demo.component.ts
@@ -3,8 +3,7 @@ import { MdDialog, MdSnackBar } from '@angular/material';
 
 @Component({
   selector: 'ng-ui-demo',
-  templateUrl: './demo.component.html',
-  styleUrls: ['./demo.component.scss']
+  templateUrl: './demo.component.html'
 })
 export class DemoComponent {
   @Input() deep: number;
@@ -22,6 +21,15 @@ export class DemoComponent {
 
 @Component({
   selector: 'ng-ui-demo-dialog',
-  template: '<em>Dialog!</em>',
+  template: `
+    <h3 md-dialog-title>Dialog Title</h3>
+    <md-dialog-content>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam at pulvinar magna, eu accumsan arcu.</p>
+      <p>Aenean sodales laoreet turpis, quis congue purus tincidunt facilisis.</p>
+    </md-dialog-content>
+    <md-dialog-actions>
+      <button md-raised-button md-dialog-close color="accent">Close <md-icon>close</md-icon></button>
+    </md-dialog-actions>
+  `
 })
 export class DemoDialogComponent {}
