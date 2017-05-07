7b96ca631ca4a31a003376d02b9f8e4f9be31cb6
Michael Cebrian <michael.cebrian@gmail.com>
2017-05-07 17:31:08 -0400

    Update for benchmark

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,18 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
-import { NglModule } from 'ng-lightning/ng-lightning';
+import {NglBadgesModule} from 'ng-lightning/badges/module';
+import {NglBreadcrumbsModule} from 'ng-lightning/breadcrumbs/module';
+import {NglButtonsModule} from 'ng-lightning/buttons/module';
+import {NglFormsModule} from 'ng-lightning/forms/module';
+import {NglIconsModule} from 'ng-lightning/icons/module';
+import {NglModalsModule} from 'ng-lightning/modals/module';
+import {NglNotificationsModule} from 'ng-lightning/notifications/module';
+import {NglPopoversModule} from 'ng-lightning/popovers/module';
+import {NglSpinnersModule} from 'ng-lightning/spinners/module';
+import {NglTabsModule} from 'ng-lightning/tabs/module';
+import {NglConfig, NGL_CONFIG} from 'ng-lightning/config/config';
+import {INglConfig} from 'ng-lightning/config/config.interface';
 
 import { AppComponent } from './app.component';
 import { DemoComponent } from './demo/demo.component';
@@ -17,9 +28,21 @@ import { DemoComponent } from './demo/demo.component';
     BrowserModule,
     FormsModule,
     HttpModule,
-    NglModule.forRoot()
+    NglBadgesModule,
+    NglBreadcrumbsModule,
+    NglButtonsModule,
+    NglFormsModule,
+    NglIconsModule,
+    NglModalsModule,
+    NglNotificationsModule,
+    NglPopoversModule,
+    NglSpinnersModule,
+    NglTabsModule
   ],
-  providers: [],
+  providers: [
+    { provide: NGL_CONFIG, useValue: <INglConfig>{} },
+    NglConfig
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/demo/demo.component.ts
+++ src/app/demo/demo.component.ts
@@ -2,17 +2,17 @@ import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'ng-ui-demo',
-  templateUrl: './demo.component.html',
-  styleUrls: ['./demo.component.scss']
+  templateUrl: './demo.component.html'
 })
 export class DemoComponent implements OnInit {
   @Input() deep: number;
   toast = false;
   popover = false;
   tab = 0;
+  breadcrumbs: number[];
   constructor() { }
 
   ngOnInit() {
+    this.breadcrumbs = Array.from(Array(this.deep + 1).keys());
   }
-
 }
